2e5b124ba475b317a1cf8710898c397f537e87a3
Bart Veneman <bartveneman@gmail.com>
2012-10-02 16:27:47 +0200

    Fixed jsonp issues

--- week5/js/deel3.js
+++ week5/js/deel3.js
@@ -1,9 +1,9 @@
 var i,
     updateList = function (data) {
         'use strict';
-        data = JSON.parse(data);
         $("ol").empty();
-        for(i = 0; i < data.results; i += 1) {
+        console.log(data);
+        for (i = 0; i < data.results.length; i += 1) {
             var li = $("<li />");
             li.text(data.results[i].text);
             li.appendTo($("ol"));
@@ -13,14 +13,14 @@ var i,
 
 $("button").on("click", function () {
     $.ajax({
-        url : "http://search.twitter.com/search.json?q=hbo&rpp=5&include_entities=true&result_type=mixed&callback=updateList",
-        crossDomain : true,
+        url : "http://search.twitter.com/search.json?q=windesheim&rpp=5&include_entities=true&result_type=mixed",
+        type : "GET",
+        dataType : "jsonp",
+        success : function (data) {
+            updateList(data);
+        },
         error : function (msg) {
             alert("error: " + msg);
-            console.log(msg);
-        },
-        success : function (data) {
-            updateList();
         }
     });
 });
