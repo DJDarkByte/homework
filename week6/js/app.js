8398d876fc99f74262e417e717c1acb2f92e86f0
Bart Veneman <bartveneman@gmail.com>
2012-10-16 10:09:05 +0200

    Completed assignment 1 of homework week 6: drawing browser shares with canvas

--- week6/js/app.js
+++ week6/js/app.js
@@ -2,21 +2,30 @@
     console.log("App start;");
     context = canvas.getContext("2d");
     
-    var drawBar = function (text, color, width, value, verticalOffet) {
+    var drawBar = function (text, color, width, value, verticalOffset) {
         // draw the bar
+        var horizontalOffset = 100;
+        var barHeight = 56;
+        
         context.fillStyle = color;
-        context.fillRect(100, 0, 400, 56);
+        context.fillRect(horizontalOffset, verticalOffset - (barHeight / 2), width, barHeight);
+        // global text settings
+        context.font = 'normal 400 16px sans-serif';
+        context.textBaseline = 'top';
+        context.fillStyle = '#000';
+        // draw browsername
+        context.fillText(text, 0, verticalOffset);
+        // draw value
+        context.fillText(value, width + horizontalOffset + 20, verticalOffset);
     }
     
+    var drawGraph = function () {
+        
+    };
+    
+    
     // First bar
-    
-    
-    // First bar text
-    context.font = 'normal 400 16px sans-serif';
-    context.textBaseline = 'top';
-    context.fillStyle = '#000';
-    context.fillText("Chrome", 0, 20);
-    
-    context.fillStyle = '#fff';
-    context.fillText("28.8%", 440, 20);
+    drawBar("Chrome", "rgba(200, 0, 0, .8)", 300, '28,8%', 20);
+    drawBar("Firefox", "rgba(0, 200, 0, .8)", 200, '20%', 56);
+    drawBar("IE", 'rgba(0, 0, 200, 0.8)', 125, '12.5%', 92);
 })();
